4a7738bec48eb982db8db3328e95434494045cea
senmontoya <alfredo.hernandez@catolica.edu.sv>
2025-02-24 18:16:55 -0600

    Agregar archivos de proyecto.

---
+++ P01_2022HM651_2022DP650/Models/parqueoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace P01_2022HM651_2022DP650.Models
+{
+    public class parqueoContext : DbContext
+    {
+        public parqueoContext(DbContextOptions<parqueoContext> options) : base(options)
+        {
+
+        }
+    }
+}
